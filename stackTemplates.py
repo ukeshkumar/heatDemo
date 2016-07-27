e02ed3735dd4b689a9cbcc838178bf1ce5c8c529
tamilhce <tamilhce@gmail.com>
2016-07-27 15:29:30 +0530

    fxing template

--- stackTemplates.py
+++ stackTemplates.py
@@ -1,4 +1,23 @@
-
+import yaml
 class stackTemplates:
-    pass
+    def __init__(self): 
+        pass 
+    def getCreateTemplate(self,clientData): 
+        parameters={}
+        para_list=['name', 'image', 'type', 'netname', 'key'] 
+        for key_name in para_list: 
+            if key_name in clientData: 
+                parameters[key_name]=clientData[key_name]
+        template=yaml.load(open('template.yaml'))
+        return template,parameters 
+
+if __name__ == "__main__" : 
+    
+    obj=stackTemplates()
+    s1=obj.getCreateTemplate({'name': 'my_int', 'key': 'pem_key', 'type' : 'instance_type'}) 
+    print s1
+        
+
+ 
+    
 
