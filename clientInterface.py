b1a53f458fb76fa56532c4d03e9ea666f9fc71e2
Ukesh Kumar Vasudevan <ukeshkumar@gmail.com>
2016-07-29 12:22:01 +0530

    site-map options to show all the available apis

--- clientInterface.py
+++ clientInterface.py
@@ -1,7 +1,7 @@
 #Heat demo
 #!/usr/bin/python
 
-from flask import Flask, request
+from flask import Flask, request, jsonify, url_for
 
 class clientInterface:
     def __init__(self, ctrObj):
@@ -10,8 +10,20 @@ class clientInterface:
         self.app.add_url_rule('/setup', None, self.setup, methods=['GET', 'POST'])
         self.app.add_url_rule('/stack/<name>', None, self.stack, methods=['GET', 'POST', 'DELETE'])
         self.app.add_url_rule('/stack/<name>/output', None, self.stackOutput, methods=['GET'])
+        self.app.add_url_rule('/', None, self.siteMap, methods=['GET'])
+
+
+    def siteMap(self):
+        """Print available apis."""
+        func_list = {}
+        for rule in self.app.url_map.iter_rules():
+            if rule.endpoint != 'static':
+                func_list[rule.rule] = self.app.view_functions[rule.endpoint].__doc__
+        return jsonify(func_list)
+
 
     def setup(self):
+        """To handle OpenStack Credentials"""
         if request.method == 'GET':
             return self.controller.getOSSetup()
         elif request.method == 'POST' and request.is_json:
@@ -20,6 +32,7 @@ class clientInterface:
             return "Error: Operation Not Supported", 405
 
     def stack(self, name=None):
+        """To Create / Delete / Get-status Stack"""
         if request.method == 'GET':
             return self.controller.getStackStatus(name)
         elif request.method == 'POST' and request.is_json:
@@ -30,6 +43,7 @@ class clientInterface:
             return "Error: Operation Not Supported", 405
 
     def stackOutput(self, name=None):
+        """To get the stack output"""
         if request.method == 'GET':
             return self.controller.getStackOutput(name)
 
